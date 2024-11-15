c4ac0eba43234e5a34fbd8c98d21c99d30d8a7cd
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-15 14:48:25 -0300

    create config classes to consume the ChatGPT API

--- src/main/java/br/com/rafaelmaia/config/OpenAIConfig.java
+++ src/main/java/br/com/rafaelmaia/config/OpenAIConfig.java
@@ -1,5 +1,15 @@
 package br.com.rafaelmaia.config;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAIConfig {
 
+	private Logger logger = Logger.getLogger(OpenAIConfig.class.getName());
+	
+	@Value("${openai.api.key}")
+	String openApiKey;
 }
