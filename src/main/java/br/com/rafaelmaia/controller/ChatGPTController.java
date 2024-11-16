a03bd27698170f28edcdf0cea8fc71604b54d335
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-16 20:18:32 -0300

    create controller responsible for allowing the consume of the ChatGPT API

---
+++ src/main/java/br/com/rafaelmaia/controller/ChatGPTController.java
@@ -0,0 +1,23 @@
+package br.com.rafaelmaia.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rafaelmaia.service.ChatGPTService;
+
+@RestController
+@RequestMapping("/bot")
+public class ChatGPTController {
+	
+	@Autowired
+	private ChatGPTService service;
+
+	// HOST/bot/chat
+	@GetMapping("/chat")
+	public Object chat(@RequestParam("prompt") String prompt) {
+		return service.chat(prompt);
+	}
+}
