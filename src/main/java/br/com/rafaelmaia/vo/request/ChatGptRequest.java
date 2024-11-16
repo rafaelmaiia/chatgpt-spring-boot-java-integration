d08cd5492748fff9bc8202fbbf7f50f16cb78378
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-16 18:33:21 -0300

    create VOs that represent the request objects to ChatGPT

---
+++ src/main/java/br/com/rafaelmaia/vo/request/ChatGptRequest.java
@@ -0,0 +1,52 @@
+package br.com.rafaelmaia.vo.request;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ChatGptRequest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String model;
+	private List<Message> messages;
+	
+	public ChatGptRequest(String model, String prompt) {
+		this.model = model;
+		this.messages = new ArrayList<>();
+		this.messages.add(new Message("user", prompt));
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(messages, model);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChatGptRequest other = (ChatGptRequest) obj;
+		return Objects.equals(messages, other.messages) && Objects.equals(model, other.model);
+	}
+}
